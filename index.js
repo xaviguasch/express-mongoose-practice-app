b4ec527de452846a534ae73d2c4058d889f55861
xaviguasch <xaviguasch@gmail.com>
2022-07-08 20:44:26 +0200

    fixed the category selector

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 const Product = require('./models/product')
 
@@ -18,10 +19,17 @@ mongoose
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'vegetable', 'dairy']
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({})
   res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new', { categories })
 })
 
 app.post('/products', async (req, res) => {
@@ -31,14 +39,26 @@ app.post('/products', async (req, res) => {
   res.redirect(`/products/${newProduct._id}`)
 })
 
-app.get('/products/new', (req, res) => {
-  res.render('products/new')
-})
-
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params
   const product = await Product.findById(id)
   res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params
+
+  const product = await Product.findById(id)
+  res.render('products/edit', { product, categories })
+})
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  })
+  res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
